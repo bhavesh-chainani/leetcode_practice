25293e8994375b9dd0c58b9ce5d31e4caced10d8
bhavesh-chainani <bhaveshchainani@gmail.com>
2022-07-21 14:11:14 +0800

    Updated python script with algo explanations.

--- telepathylabs_bhavesh.py
+++ telepathylabs_bhavesh.py
@@ -2,15 +2,17 @@
 Explanation of Algorithm
 
 Approach: The idea is to use Breadth First Search. The condition of patients getting infected is when they come in contact with other infected patients. This is similar to breadth-first search where the graph is divided into layers or circles and the search is done from lower or closer layers to deeper or higher layers.  To find the elements whose values are no the whole matrix had to be traversed.
- 
-Algorithm: 
-Create an empty queue q. 
-Find all infected victims and enqueue them to q. Also, keep a time variable to keep track of time passed
-Run a loop while q is not empty and number of uninfected patients is more than 0
-Dequeue a patient from the queue (popleft), infect all adjacent patients. While infecting the adjacent, make sure that the time frame is incremented only once. And the time frame is not incremented if there are no adjacent patients.
-Append newly infected patients to queue and continue till while loop breaks
-Return time if every single patient becomes infected, else return -1
+
 '''
+
+# Algorithm: 
+# 1. Create an empty queue q. 
+# 2. Find all infected victims and enqueue them to q. Also, keep a time variable to keep track of time passed.
+# 3. Run a loop while q is not empty and number of uninfected patients is more than 0.
+# 4. Dequeue a patient from the queue (popleft), infect all adjacent patients. While infecting the adjacent, make sure that the time frame is incremented only once, and the time frame is not incremented if there are no adjacent patients.
+# 5. Append newly infected patients to queue and continue till while loop breaks
+# 6. Return time if every single patient becomes infected, else return -1
+
 
 from collections import deque
 
